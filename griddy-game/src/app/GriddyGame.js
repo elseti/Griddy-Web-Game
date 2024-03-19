1775ae843eb174fd08b57dc7f0a486604e3df963
Eliana Setiabudi <amidis100@gmail.com>
2024-03-19 19:16:46 +0800

    feat: implemented timer countdown

--- griddy-game/src/app/GriddyGame.js
+++ griddy-game/src/app/GriddyGame.js
@@ -5,12 +5,29 @@ import { createGridArray } from "@/utils/GameLogic";
 
 export default function GriddyGame(props){
     const [gridArray, setGridArray] = useState(null);
+    const [inputArray, setInputArray] = useState(null);
+    const [isTimerDone, setTimerDone] = useState(false);
+    const [timerValue, setTimerValue] = useState(10);
+
     const gridNo = props.gridNo;
     const greenSquareNo = props.greenSquareNo;
 
     useEffect(() => {
         createGrid(gridNo, greenSquareNo);
     }, []);
+
+    // Timer countdown
+    useEffect(() => {
+        if(timerValue > 0){
+            const interval = setInterval(() => {
+                setTimerValue(prevTimerValue => prevTimerValue - 1);
+            }, 1000);
+            return () => clearInterval(interval);
+        }
+        if(timerValue === 0){
+            setTimerDone(true);
+        }
+    }, [timerValue]);
 
     const createGrid = async(e) => {
         let grid = createGridArray(gridNo, greenSquareNo);
@@ -20,7 +37,7 @@ export default function GriddyGame(props){
                 divArray.push(<div className="bg-green-500 p-10"/>);
             }
             else{
-                divArray.push(<div className="bg-red-500 p-10"/>);
+                divArray.push(<div className="bg-gray-500 p-10"/>);
             }
         }
         await setGridArray(divArray);
@@ -29,18 +46,38 @@ export default function GriddyGame(props){
     return (
         <>
             <div className="flex flex-col justify-center bg-teal-200 text-black items-center h-screen">
+                <div className="text-3xl p-10">
+                    <p>{timerValue}</p>
+                </div>
                 <div className="grid grid-cols-4 gap-4">
-                {gridArray !== null ? (
-                    gridArray.map((box, index) => (
-                        <div key={index}>
-                            {box}
+                {!isTimerDone ? (
+                    gridArray !== null ? (
+                        gridArray.map((box, index) => (
+                            <div key={index}>
+                                {box}
+                            </div>
+                        ))
+                    ) : (
+                        <div className="flex flex-col justify-center bg-teal-200 text-black items-center h-screen">
+                            <p>Loading...</p>
                         </div>
-                    ))
+                    )
                 ) : (
-                    <></>
+                    gridArray !== null ? (
+                        gridArray.map((box, index) => (
+                            <div key={index}>
+                                <div className="bg-gray-300 hover:bg-yellow-300 p-10"/>
+                            </div>
+                        ))
+                    ) : (
+                        <>
+                        <div className="flex flex-col justify-center bg-teal-200 text-black items-center h-screen">
+                            <p>Loading...</p>
+                        </div>
+                        </>
+                    )
                 )}
-                    
-                    
+                
                 </div>
             </div>
         </>
